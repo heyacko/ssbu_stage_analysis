9a5ae5e4beae6607f47b906707b1447f029c28d7
nick <heyitsbreakfast@gmail.com>
2022-07-20 18:48:40 -0700

    more uploads

--- stage_char_analysis.R
+++ stage_char_analysis.R
@@ -11,47 +11,127 @@ con <- GraphqlClient$new(url = url,
                          headers= list('Authorization'= paste0("Bearer ", gg_key),
                                        'Content-Type'= 'application/json'))
 ###############################################################################
-
-game_df_21_jan_h1 <- tournaments_21 %>% 
-  dplyr::filter(!is.na(numAttendees)) %>%
-  dplyr::filter(start_date <= "2021-01-15") %>%
-  dplyr::select(id) %>% 
-  pull() %>%
-  lapply(pull_game_info_from_pages) %>%
-  lapply(tidy_game_data) %>%
-  bind_rows()
-
-game_df_21_jan_h1 %>% write.csv("game_df_21_jan_h1.csv")
-t %>% write.csv("tournaments_21.csv")
-
-game_df_21_jan_h2 <- tournaments_21 %>% 
-  dplyr::filter(!is.na(numAttendees)) %>%
-  dplyr::filter(start_date > "2021-01-15") %>%
-  dplyr::filter(start_date <= "2021-01-31") %>%
-  dplyr::select(id) %>%
-  pull() %>%
-  create_games_df_from_ids_list()
-
-game_df_21_jan_h2_tbl <- game_df_21_jan_h2 %>%
-  lapply(parse_tournament_object) %>%
-  remove_noncompliant_tournaments_from_list() %>%
-  lapply(tidy_game_data) %>%
-  bind_rows()
-
-game_df_21_jan_h2_tbl %>%
-  remove_noncompliant_tournaments_from_list() %>%
-  lapply(tidy_game_data) %>%
-  bind_rows()
-
-test %>%
-  lapply(as.tibble)
-
-test <- pull_ids_from_tourney_df(tournaments_21, "2021-02-01", "2021-02-15") %>%
+# initial pull of tournament objects
+games_jan_h1 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-01-01", "2021-01-15") %>%
   lapply(pull_tournament_res_from_id)
-  
-t <- test %>%
-  lapply(combine_pages_from_object)
-  
+
+games_jan_h2 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-01-16", "2021-01-31") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_feb_h1 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-02-01", "2021-02-15") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_feb_h2 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-02-16", "2021-02-28") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_mar_h1 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-03-01", "2021-03-15") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_mar_h2 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-03-16", "2021-03-31") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_apr_h1 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-04-01", "2021-04-15") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_apr_h2 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-04-16", "2021-04-30") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_may_h1 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-05-01", "2021-05-15") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_may_h2 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-05-16", "2021-05-31") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_jun_h1 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-06-01", "2021-06-15") %>%
+  lapply(pull_tournament_res_from_id)
+
+games_jun_h2 <-
+  pull_ids_from_tourney_df(tournaments_21, "2021-06-16", "2021-06-30") %>%
+  lapply(pull_tournament_res_from_id)
+
+###############################################################################
+
+games_jan_h1 <- games_jan_h1 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_jan_h1 %>% write.csv("data/games_jan_h1_21.csv")
+
+games_feb_h1 <- games_feb_h1 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_feb_h1 %>%
+  write.csv("data/games_feb_h1_21.csv")
+
+games_feb_h2 <- games_feb_h2 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_feb_h2 %>%
+  write.csv("data/games_feb_h2_21.csv")
+
+games_mar_h1 <- games_mar_h1 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_mar_h1 %>%
+  write.csv("data/games_mar_h1_21.csv")
+
+games_apr_h1 <- games_apr_h1 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_apr_h1 %>%
+  write.csv("data/games_apr_h1_21.csv")
+
+games_apr_h2 <- games_apr_h2 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_apr_h2 %>%
+  write.csv("data/games_apr_h2_21.csv")
+
+games_may_h2 <- games_may_h2 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_may_h2 %>%
+  write.csv("data/games_may_h2_21.csv")
+
+games_jun_h1 <- games_jun_h1 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_jun_h1 %>%
+  write.csv("data/games_jun_h1_21.csv")
+
+games_jun_h2 <- games_jun_h2 %>%
+  lapply(combine_pages_from_object) %>%
+  lapply(discard_noncompliant_dataframes) %>%
+  bind_rows()
+
+games_jun_h2 %>%
+  write.csv("data/games_jun_h2_21.csv")
 
 combine_pages_from_object <- function(tournament_object) {
   
@@ -63,47 +143,61 @@ combine_pages_from_object <- function(tournament_object) {
   
   print(paste0("Current ID: ", tournament_id))
   
-  unnested_df <- tournament_object %>%
-    lapply(as.tibble, .name_repair= "unique") %>%
-    bind_rows() %>%
-    unnest(everything(), names_repair = "unique") %>%
-    unnest(everything(), names_repair = "unique") %>%
-    unnest(everything(), names_repair = "unique") %>%
-    unnest(everything(), names_repair = "unique") %>%
-    unnest(everything(), names_repair = "unique") %>%
-    unnest(everything(), names_repair = "unique") %>%
-    bind_rows()
+  unnested_df <- tryCatch(
+    tournament_object %>%
+      lapply(as.tibble, .name_repair = "unique") %>%
+      bind_rows() %>%
+      unnest(everything(), names_repair = "unique") %>%
+      unnest(everything(), names_repair = "unique") %>%
+      unnest(everything(), names_repair = "unique") %>%
+      unnest(everything(), names_repair = "unique") %>%
+      unnest(everything(), names_repair = "unique") %>%
+      unnest(everything(), names_repair = "unique") %>%
+      bind_rows(),
+    error = function(e) {
+      message(paste0("Error at ", tournament_id))
+    }
+  )
   
-  if (ncol(unnested_df) == 23 && !"stage" %in% names(unnested_df)){
-    unnested_df %>%
-      rename(
-      tournament_id = id...1,
-      tournament_name = name...2,
-      attendee_count = numAttendees,
-      start_date = startAt,
-      end_date = endAt,
-      event_id = id...6,
-      event_state = state,
-      is_online = isOnline,
-      event_name = name...9,
-      competition_tier = competitionTier,
-      event_type = type,
-      set_id = id...12,
-      set_round = round,
-      set_name = fullRoundText,
-      match_id = id...15,
-      match_round = orderNum,
-      selection_id = id...17,
-      selection_type = selectionType,
-      selection_value = selectionValue,
-      player_id = id...20,
-      player_name = name...21,
-      winner_id = winnerId,
-      stage_name = name...23
-    ) %>%
-    return()
-  } else if ("stage" %in% names(unnested_df)) {
+  return(unnested_df)
+}
+
+discard_noncompliant_dataframes <- function(tournament_df) {
+  
+  if (is.null(tournament_df)) {
     return(NULL)
+  }
+  
+  print(distinct(tournament_df %>% dplyr::select(id...1)) %>% pull())
+  
+  if (ncol(tournament_df) == 23 && !"stage" %in% names(tournament_df)){
+    tournament_df %>%
+      rename(
+        tournament_id = id...1,
+        tournament_name = name...2,
+        attendee_count = numAttendees,
+        start_date = startAt,
+        end_date = endAt,
+        event_id = id...6,
+        event_state = state,
+        is_online = isOnline,
+        event_name = name...9,
+        competition_tier = competitionTier,
+        event_type = type,
+        set_id = id...12,
+        set_round = round,
+        set_name = fullRoundText,
+        match_id = id...15,
+        match_round = orderNum,
+        selection_id = id...17,
+        selection_type = selectionType,
+        selection_value = selectionValue,
+        player_id = id...20,
+        player_name = name...21,
+        winner_id = winnerId,
+        stage_name = name...23
+      ) %>%
+      return()
   } else {
     return(NULL)
   }
